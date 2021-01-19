520953e7715b422ac0176623045f505a8ec28b92
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-01-19 16:30:51 +0100

    Add Builder and Prototype patterns.

---
+++ scr/src/MyProject/Builder/TestBuilderPattern.java
@@ -0,0 +1,19 @@
+package MyProject.Builder;
+
+/**
+ * @author Hamed Adeli,
+ * h.adeli.meadeli@gmail.com
+ * 1/19/2021
+ */
+public class TestBuilderPattern {
+
+    public static void main(String[] args) {
+        //Using builder to get the object in a single line of code and
+        //without any inconsistent state or arguments management issues
+        Computer comp = new Computer.ComputerBuilder(
+                "500 GB", "2 GB").setBluetoothEnabled(true)
+                .setGraphicsCardEnabled(true).build();
+        System.out.println(comp);
+    }
+
+}
---
+++ scr/src/MyProject/Prototype/Employees.java
@@ -0,0 +1,43 @@
+package MyProject.Prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Hamed Adeli,
+ * h.adeli.meadeli@gmail.com
+ * 1/19/2021
+ */
+public class Employees implements Cloneable{
+
+    private List<String> empList;
+
+    public Employees(){
+        empList = new ArrayList<String>();
+    }
+
+    public Employees(List<String> list){
+        this.empList=list;
+    }
+    public void loadData(){
+        //read all employees from database and put into the list
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("David");
+        empList.add("Lisa");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+        List<String> temp = new ArrayList<String>();
+        for(String s : this.getEmpList()){
+            temp.add(s);
+        }
+        return new Employees(temp);
+    }
+
+}
